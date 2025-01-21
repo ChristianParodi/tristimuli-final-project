744e0f7bbcd332b3118a7e953b9978d13e355293
Celeste <celeste.harleybazzi@gmail.com>
2025-01-21 12:08:49 +0100

    modifica al clean covid(aggiunto uno), miglioramento linechart, aggiustamento butterfly

--- src/js/covidspread/linechart.js
+++ src/js/covidspread/linechart.js
@@ -1,141 +1,137 @@
 function LineChart() {
-    d3.csv("./../../../dataset/COVID/covid.csv", d => ({
+  d3.csv("./../../../dataset/COVID/covid_test.csv", d => ({
       country: d.country,
       year: +d.year,
       month: +d.month,
       total_cases: +d.total_cases,
-    })).then(covidData => {
-      // Raggruppa i dati per paese
+  })).then(covidData => {
       const groupedData = d3.group(covidData, d => d.country);
-  
-      // Trasforma i dati in un formato più adatto
+
       const parsedData = Array.from(groupedData, ([country, values]) => ({
-        country,
-        values: values.map(d => ({
-          date: new Date(d.year, d.month - 1),
-          total_cases: d.total_cases,
-        })),
+          country,
+          values: values.map(d => ({
+              date: new Date(d.year, d.month - 1),
+              total_cases: d.total_cases,
+          })),
       }));
-  
-      // Configurazione del grafico
+
       const margin = { top: 20, right: 30, bottom: 50, left: 60 };
       const width = 800 - margin.left - margin.right;
       const height = 400 - margin.top - margin.bottom;
-  
-      const svg = d3.select("#chart-container")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-  
-      // Scala del tempo (asse x)
-      const x = d3.scaleTime()
-        .domain(d3.extent(covidData, d => new Date(d.year, d.month - 1)))
-        .range([0, width]);
-  
-      const xAxis = d3.axisBottom(x)
-        .ticks(d3.timeMonth.every(3))
-        .tickFormat(d3.timeFormat("%b %Y"));
-  
-      // Aggiungi l'asse x
-      svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(xAxis)
-        .style("color", "#ccc")
-        .selectAll("text")
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "end")
-        .style("fill", "black");
-  
-      // Scala delle infezioni (asse y)
-      const y = d3.scaleLinear()
-        .range([height, 0]);
-  
-      const yAxisGroup = svg.append("g")
-        .attr("class", "y-axis");
-  
-      // Griglia orizzontale
-      const horizontalGrid = svg.append("g")
-        .attr("class", "grid-horizontal");
-  
-      // Griglia verticale
-      const verticalGrid = svg.append("g")
-        .attr("class", "grid-vertical");
-  
-      // Linea del grafico
-      const line = d3.line()
-        .x(d => x(d.date))
-        .y(d => y(d.total_cases));
-  
-      // Funzione per aggiornare il grafico in base al paese selezionato
-      function updateChart(country) {
-        const countryData = parsedData.find(d => d.country === country);
-  
-        // Determina il dominio dell'asse Y
-        const maxTotalCases = d3.max(countryData.values, d => d.total_cases);
-        console.log(maxTotalCases)
-        y.domain([0, d3.max(covidData, d => d.total_cases)]).nice();
-        if (maxTotalCases < '500000') {
-          y.domain([0, maxTotalCases]).nice();
-        } 
 
-        
-  
-        // Aggiorna l'asse Y
-        yAxisGroup.call(d3.axisLeft(y))
+      const svg = d3.select("#chart-container")
+          .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", `translate(${margin.left},${margin.top})`);
+
+      const x = d3.scaleTime()
+          .domain(d3.extent(covidData, d => new Date(d.year, d.month - 1)))
+          .range([0, width]);
+
+      const xAxis = d3.axisBottom(x)
+          .ticks(d3.timeMonth.every(3))
+          .tickFormat(d3.timeFormat("%b %Y"));
+
+      svg.append("g")
+          .attr("transform", `translate(0,${height})`)
+          .call(xAxis)
+          .style("color", "#ccc")
           .selectAll("text")
+          .attr("transform", "rotate(-45)")
+          .style("text-anchor", "end")
           .style("fill", "black");
-  
-        // Aggiorna la griglia orizzontale
-        horizontalGrid.call(
-          d3.axisLeft(y)
-            .tickSize(-width)
-            .tickFormat("")
-        ).selectAll("line")
-          .style("stroke", "#ccc")
-          .style("stroke-opacity", 0.7)
-          .style("shape-rendering", "crispEdges");
-  
-        // Aggiorna la griglia verticale
-        verticalGrid.call(
-          d3.axisBottom(x)
-            .tickSize(-height)
-            .tickFormat("")
-        ).attr("transform", `translate(0, ${height})`)
-          .selectAll("line")
-          .style("stroke", "#ccc")
-          .style("stroke-opacity", 0.7)
-          .style("shape-rendering", "crispEdges");
-  
-        // Rimuove la linea precedente
-        svg.selectAll(".line").remove();
-  
-        // Aggiunge la nuova linea
-        svg.append("path")
-          .datum(countryData.values)
-          .attr("class", "line")
-          .attr("fill", "none")
-          .attr("stroke", "steelblue")
-          .attr("stroke-width", 2)
-          .attr("d", line);
+
+      const y = d3.scaleLinear()
+          .range([height, 0]);
+
+      const yAxisGroup = svg.append("g").attr("class", "y-axis");
+      const horizontalGrid = svg.append("g").attr("class", "grid-horizontal");
+
+      const line = d3.line()
+          .x(d => x(d.date))
+          .y(d => y(d.total_cases));
+
+      const tooltip = d3.select("body")
+          .append("div")
+          .style("position", "absolute")
+          .style("background-color", "white")
+          .style("padding", "5px")
+          .style("border", "1px solid #ccc")
+          .style("border-radius", "5px")
+          .style("pointer-events", "none")
+          .style("display", "none")
+          .style("font-size", "12px");
+
+      function updateChart(country) {
+          const countryData = parsedData.find(d => d.country === country);
+
+          const maxTotalCases = d3.max(countryData.values, d => d.total_cases);
+          y.domain([0, maxTotalCases]).nice();
+
+          yAxisGroup.call(d3.axisLeft(y))
+              .selectAll("text")
+              .style("fill", "black");
+
+          horizontalGrid.call(
+              d3.axisLeft(y)
+                  .tickSize(-width)
+                  .tickFormat("")
+          ).selectAll("line")
+              .style("stroke", "#ccc")
+              .style("stroke-opacity", 0.7)
+              .style("shape-rendering", "crispEdges");
+
+          svg.selectAll(".line").remove();
+          svg.selectAll(".point").remove();
+
+          svg.append("path")
+              .datum(countryData.values)
+              .attr("class", "line")
+              .attr("fill", "none")
+              .attr("stroke", "steelblue")
+              .attr("stroke-width", 2)
+              .attr("d", line);
+
+          svg.selectAll(".point")
+              .data(countryData.values)
+              .enter()
+              .append("circle")
+              .attr("class", "point")
+              .attr("cx", d => x(d.date))
+              .attr("cy", d => y(d.total_cases))
+              .attr("r", 4)
+              .attr("fill", "steelblue")
+              .on("mouseover", function (event, d) {
+                  tooltip.style("display", "block")
+                        .style("color","black")
+                      .html(`Date: ${d.date.toLocaleDateString()}<br>Total Cases: ${d.total_cases}`);
+                  d3.select(this).attr("r", 6).attr("fill", "orange");
+              })
+              .on("mousemove", function (event) {
+                  tooltip.style("left", `${event.pageX + 10}px`)
+                      .style("top", `${event.pageY - 20}px`);
+              })
+              .on("mouseout", function () {
+                  tooltip.style("display", "none");
+                  d3.select(this).attr("r", 4).attr("fill", "steelblue");
+              });
       }
-      // Dropdown per selezionare il paese
+
       const selector = d3.select("#country-selector")
-        .on("change", function() {
-          updateChart(this.value);
-        });
-  
+          .on("change", function () {
+              updateChart(this.value);
+          });
+
       selector.selectAll("option")
-        .data(parsedData.map(d => d.country))
-        .enter()
-        .append("option")
-        .text(d => d);
-  
-      // Disegna il grafico iniziale
+          .data(parsedData.map(d => d.country))
+          .enter()
+          .append("option")
+          .text(d => d);
+
       updateChart(parsedData[0].country);
-    });
-  }
-  
-  LineChart();
-  
+  });
+}
+
+LineChart();
--- src/js/economy/butterflychart.js
+++ src/js/economy/butterflychart.js
@@ -1,92 +1,103 @@
 function ButterflyChart() {
-    // Data for countries and tourism
-    const data = [
-      { country: "USA", intourism: 120, outtourism: 80 },
-      { country: "France", intourism: 150, outtourism: 110 },
-      { country: "Germany", intourism: 100, outtourism: 75 },
-      { country: "Japan", intourism: 90, outtourism: 60 },
-      { country: "UK", intourism: 130, outtourism: 90 },
-      // Add more countries and their tourism data
-    ];
+  // Data for countries and tourism
+  const data = [
+      { country: "USA", intourism: 120, outtourism: 80, year: 2020 },
+      { country: "France", intourism: 150, outtourism: 110, year: 2020 },
+      { country: "Germany", intourism: 100, outtourism: 75, year: 2020 },
+      { country: "Japan", intourism: 90, outtourism: 60, year: 2020 },
+      { country: "UK", intourism: 130, outtourism: 90, year: 2020 },
+      { country: "USA", intourism: 140, outtourism: 90, year: 2021 },
+      { country: "France", intourism: 160, outtourism: 120, year: 2021 },
+      { country: "Germany", intourism: 110, outtourism: 85, year: 2021 },
+      { country: "Japan", intourism: 100, outtourism: 70, year: 2021 },
+      { country: "UK", intourism: 135, outtourism: 95, year: 2021 },
+  ];
 
-    const margin = { top: 40, right: 20, bottom: 40, left: 200 };
-    const width = 800;
-    const height = 500;
+  const margin = { top: 40, right: 20, bottom: 40, left: 200 };
+  const width = 800;
+  const height = 400;
 
-    const svg = d3.select("#butterflychart_container")
-    .append("svg")
-      .attr("width", width)
-      .attr("height", height);
+  // Group data by year
+  const dataByYear = d3.group(data, d => d.year);
 
-    // Set up scales
-    const yScale = d3.scaleBand()
-      .domain(data.map(d => d.country))
-      .range([margin.top, height - margin.bottom])
-      .padding(0.1);
+  // Create a container for each year
+  const container = d3.select("#butterflychart_container");
 
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => Math.max(d.intourism, d.outtourism))])
-      .range([margin.left, width - margin.right]);
+  dataByYear.forEach((yearData, year) => {
+      const svg = container.append("div")
+          .style("margin-bottom", "50px")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height);
 
-    // Append axes
-    svg.append("g")
-      .selectAll(".y-axis")
-      .data(data)
-      .enter()
-      .append("text")
-      .attr("class", "label")
-      .attr("x", margin.left - 10)
-      .attr("y", d => yScale(d.country) + yScale.bandwidth() / 2)
-      .attr("dy", "0.35em")
-      .attr("text-anchor", "end")
-      .text(d => d.country);
+      // Set up scales
+      const yScale = d3.scaleBand()
+          .domain(yearData.map(d => d.country))
+          .range([margin.top, height - margin.bottom])
+          .padding(0.1);
 
-    // Left side bars (intourism)
-    svg.selectAll(".bar-left")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", margin.left)
-      .attr("y", d => yScale(d.country))
-      .attr("width", d => xScale(d.intourism) - margin.left)
-      .attr("height", yScale.bandwidth())
-      .attr("fill", "#ff8c00");
+      const xScale = d3.scaleLinear()
+          .domain([0, d3.max(yearData, d => Math.max(d.intourism, d.outtourism))])
+          .range([margin.left, width - margin.right]);
 
-    // Right side bars (outtourism)
-    svg.selectAll(".bar-right")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", d => xScale(d.outtourism))
-      .attr("y", d => yScale(d.country))
-      .attr("width", d => xScale(d.intourism) - xScale(d.outtourism))
-      .attr("height", yScale.bandwidth())
-      .attr("fill", "#6a3d9a");
+      // Append axes
+      svg.append("g")
+          .selectAll(".y-axis")
+          .data(yearData)
+          .enter()
+          .append("text")
+          .attr("class", "label")
+          .attr("x", margin.left - 10)
+          .attr("y", d => yScale(d.country) + yScale.bandwidth() / 2)
+          .attr("dy", "0.35em")
+          .attr("text-anchor", "end")
+          .text(d => d.country);
 
-    // Title
-    svg.append("text")
-      .attr("class", "title")
-      .attr("x", width / 2)
-      .attr("y", margin.top / 2)
-      .attr("text-anchor", "middle")
-      .text("Tourism Comparison by Country (Intourism vs Outtourism)");
+      // Left side bars (intourism)
+      svg.selectAll(".bar-left")
+          .data(yearData)
+          .enter()
+          .append("rect")
+          .attr("class", "bar")
+          .attr("x", margin.left)
+          .attr("y", d => yScale(d.country))
+          .attr("width", d => xScale(d.intourism) - margin.left)
+          .attr("height", yScale.bandwidth())
+          .attr("fill", "#ff8c00");
 
-    // Axis Labels
-    svg.append("text")
-      .attr("class", "label")
-      .attr("x", width / 2)
-      .attr("y", height - margin.bottom + 30)
-      .attr("text-anchor", "middle")
-      .text("Tourism Numbers");
+      // Right side bars (outtourism)
+      svg.selectAll(".bar-right")
+          .data(yearData)
+          .enter()
+          .append("rect")
+          .attr("class", "bar")
+          .attr("x", d => xScale(d.outtourism))
+          .attr("y", d => yScale(d.country))
+          .attr("width", d => xScale(d.intourism) - xScale(d.outtourism))
+          .attr("height", yScale.bandwidth())
+          .attr("fill", "#6a3d9a");
 
-    // Axis lines (optional)
-    svg.append("g")
-      .attr("transform", `translate(0, ${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale));
+      // Title
+      svg.append("text")
+          .attr("class", "title")
+          .attr("x", width / 2)
+          .attr("y", margin.top / 2)
+          .attr("text-anchor", "middle")
+          .text(`Tourism Comparison by Country (${year})`);
 
+      // Axis Labels
+      svg.append("text")
+          .attr("class", "label")
+          .attr("x", width / 2)
+          .attr("y", height - margin.bottom + 30)
+          .attr("text-anchor", "middle")
+          .text("Tourism Numbers");
 
+      // Axis lines (optional)
+      svg.append("g")
+          .attr("transform", `translate(0, ${height - margin.bottom})`)
+          .call(d3.axisBottom(xScale));
+  });
 }
 
 ButterflyChart();
