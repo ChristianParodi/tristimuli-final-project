e380adbc8b1439e42129f3ecc0d9a13570dd9ed5
Christian Parodi <S4951409@studenti.unige.it>
2025-01-23 11:42:03 +0100

    updated covid start date, added datasets for unemployment and education data loading

--- src/js/education_jobs/dumbbellEnrollments.js
+++ src/js/education_jobs/dumbbellEnrollments.js
@@ -1,183 +1,181 @@
-import { covidDates } from '../utils.js'
+import { covidDates, datasets } from '../utils.js'
 
 function dumbbellUnenployments() {
 
-  d3.csv("../../../dataset/EDUCATION/clean/estat_educ_uoe_enrt02.csv")
-    .then(educationData => {
+  const educationData = datasets.educationData;
 
-      const MAX_ENROLLMENTS_COUNTRY = "Germany";
-      const years = d3.range(2016, 2023);
-      let selectedCountry = "Italy";
-      let selectedLevel = "Tertiary education (levels 5-8)";
-      let selectedAge = "Total";
-      let sexIsTotal = false;
+  const MAX_ENROLLMENTS_COUNTRY = "Germany";
+  const years = d3.range(2016, 2023);
+  let selectedCountry = "Italy";
+  let selectedLevel = "Tertiary education (levels 5-8)";
+  let selectedAge = "Total";
+  let sexIsTotal = false;
 
-      const selector = d3.select("#country-selector-dumbbell-enrollments");
-      const allCountries = Array.from(new Set(educationData.map(d => d.country)));
+  const selector = d3.select("#country-selector-dumbbell-enrollments");
+  const allCountries = Array.from(new Set(educationData.map(d => d.country)));
 
-      selector.selectAll("option")
-        .data(allCountries)
-        .enter()
-        .append("option")
-        .text(d => d)
-        .property("selected", d => d === selectedCountry);
+  selector.selectAll("option")
+    .data(allCountries)
+    .enter()
+    .append("option")
+    .text(d => d)
+    .property("selected", d => d === selectedCountry);
 
-      const processedData = educationData.flatMap(d =>
-        years.map(year => ({
-          country: d.country,
-          sex: d.sex,
-          age: d.age,
-          year: year,
-          level: d.level,
-          enrollments: +d[year]
-        }))
-      );
+  const processedData = educationData.flatMap(d =>
+    years.map(year => ({
+      country: d.country,
+      sex: d.sex,
+      age: d.age,
+      year: year,
+      level: d.level,
+      enrollments: +d[year]
+    }))
+  );
 
-      const margin = { top: 20, right: 20, bottom: 40, left: 120 };
-      const width = 800 - margin.left - margin.right;
-      const height = 600 - margin.top - margin.bottom;
+  const margin = { top: 20, right: 20, bottom: 40, left: 120 };
+  const width = 800 - margin.left - margin.right;
+  const height = 600 - margin.top - margin.bottom;
 
-      const svg = d3.select("#dumbbell-enrollments")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
-      const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+  const svg = d3.select("#dumbbell-enrollments")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+  const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-      const xScale = d3.scaleBand()
-        .domain(years)
-        .range([0, width])
-        .padding(0.2);
+  const xScale = d3.scaleBand()
+    .domain(years)
+    .range([0, width])
+    .padding(0.2);
 
-      const getMaxEnrollments = () => {
-        const targetCountry = (selectedCountry === "Turkey" || selectedCountry === "European Union") ? selectedCountry : MAX_ENROLLMENTS_COUNTRY;
+  const getMaxEnrollments = () => {
+    const targetCountry = (selectedCountry === "Turkey" || selectedCountry === "European Union") ? selectedCountry : MAX_ENROLLMENTS_COUNTRY;
 
-        const maxEnr = d3.max(years, year => {
-          const yearData = processedData.filter(d => d.year === year && d.country === targetCountry && d.level === selectedLevel && d.age === selectedAge);
-          if (sexIsTotal) {
-            return d3.max(yearData, d => d.enrollments);
-          } else {
-            const maleEnrollments = d3.max(yearData.filter(d => d.sex === "Males"), d => d.enrollments);
-            const femaleEnrollments = d3.max(yearData.filter(d => d.sex === "Females"), d => d.enrollments);
-            return d3.max([maleEnrollments, femaleEnrollments]);
-          }
-        });
-        return maxEnr;
-      };
-
-      const covidStartX = xScale(covidDates.start.getFullYear()) + xScale.bandwidth() / 2;
-      const covidEndX = xScale(covidDates.end.getFullYear()) + xScale.bandwidth() / 2;
-
-      const yScale = d3.scaleLinear()
-        .domain([0, getMaxEnrollments()])
-        .range([height, 0]);
-
-      g.append("g")
-        .attr("class", "xaxis")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(xScale)
-          .tickFormat(d3.format("d"))
-          .tickValues(years))
-        .selectAll("text")
-        .style("font-size", "14px")
-        .style("fill", "black");
-
-      g.append("g")
-        .attr("class", "yaxis")
-        .call(d3.axisLeft(yScale))
-        .selectAll("text")
-        .style("font-size", "14px")
-        .style("fill", "black");
-
-      // Y label
-      g.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .style("fill", "black")
-        .style("font-size", "18px")
-        .text("Tertiary education enrollments");
-
-      function updateChart(country) {
-        easeOutLines(g);
-
-        // covid lines
-        g.append("line")
-          .attr("x1", covidStartX)
-          .attr("x2", covidStartX)
-          .attr("y1", 0)
-          .attr("y2", height)
-          .attr("stroke", "black")
-          .attr("stroke-width", 2)
-          .attr("stroke-dasharray", "4");
-
-        // label
-        g.append("text")
-          .attr("x", covidStartX)
-          .attr("y", -10)
-          .attr("text-anchor", "middle")
-          .attr("fill", "black")
-          .style("font-size", "12px")
-          .text("COVID Starts");
-
-        // we do not have 2023
-        // g.append("line")
-        //   .attr("x1", covidEndX)
-        //   .attr("x2", covidEndX)
-        //   .attr("y1", 0)
-        //   .attr("y2", height)
-        //   .attr("stroke", "black")
-        //   .attr("stroke-width", 2)
-        //   .attr("stroke-dasharray", "4");
-
-        // // label
-        // g.append("text")
-        //   .attr("x", covidEndX)
-        //   .attr("y", -10)
-        //   .attr("text-anchor", "middle")
-        //   .attr("fill", "black")
-        //   .style("font-size", "12px")
-        //   .text("COVID Ends");
-
-        const filteredData = processedData.filter(d => d.level === selectedLevel && d.country === country);
-        const maleData = filteredData.filter(d => d.sex === "Males" && d.age === "Total");
-        const femaleData = filteredData.filter(d => d.sex === "Females" && d.age === "Total");
-
-        yScale.domain([0, getMaxEnrollments()]);
-
-        g.select(".yaxis")
-          .transition()
-          .duration(1000)
-          .ease(d3.easeCubicInOut)
-          .call(d3.axisLeft(yScale))
-          .selectAll("text")
-          .style("font-size", "14px")
-          .style("fill", "black");
-
-        femaleData.forEach((female, i) => {
-          const male = maleData[i];
-          drawLines(g, male, female, xScale, yScale);
-        });
-
-        const maleDots = g.selectAll(".dot.male")
-          .data(maleData);
-
-        drawMalePoints(maleDots, xScale, yScale);
-
-        const femaleDots = g.selectAll(".dot.female")
-          .data(femaleData);
-
-        drawFemalePoints(femaleDots, xScale, yScale);
+    const maxEnr = d3.max(years, year => {
+      const yearData = processedData.filter(d => d.year === year && d.country === targetCountry && d.level === selectedLevel && d.age === selectedAge);
+      if (sexIsTotal) {
+        return d3.max(yearData, d => d.enrollments);
+      } else {
+        const maleEnrollments = d3.max(yearData.filter(d => d.sex === "Males"), d => d.enrollments);
+        const femaleEnrollments = d3.max(yearData.filter(d => d.sex === "Females"), d => d.enrollments);
+        return d3.max([maleEnrollments, femaleEnrollments]);
       }
-
-      updateChart(selectedCountry);
-
-      selector.on("change", function () {
-        selectedCountry = this.value;
-        updateChart(this.value);
-      });
     });
+    return maxEnr;
+  };
+
+  const covidStartX = xScale(covidDates.start.getFullYear()) + xScale.bandwidth() / 2;
+  const covidEndX = xScale(covidDates.end.getFullYear()) + xScale.bandwidth() / 2;
+
+  const yScale = d3.scaleLinear()
+    .domain([0, getMaxEnrollments()])
+    .range([height, 0]);
+
+  g.append("g")
+    .attr("class", "xaxis")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale)
+      .tickFormat(d3.format("d"))
+      .tickValues(years))
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("fill", "black");
+
+  g.append("g")
+    .attr("class", "yaxis")
+    .call(d3.axisLeft(yScale))
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("fill", "black");
+
+  // Y label
+  g.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("fill", "black")
+    .style("font-size", "18px")
+    .text("Tertiary education enrollments");
+
+  function updateChart(country) {
+    easeOutLines(g);
+
+    // covid lines
+    g.append("line")
+      .attr("x1", covidStartX)
+      .attr("x2", covidStartX)
+      .attr("y1", 0)
+      .attr("y2", height)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", "4");
+
+    // label
+    g.append("text")
+      .attr("x", covidStartX)
+      .attr("y", -10)
+      .attr("text-anchor", "middle")
+      .attr("fill", "black")
+      .style("font-size", "12px")
+      .text("COVID Starts");
+
+    // we do not have 2023
+    // g.append("line")
+    //   .attr("x1", covidEndX)
+    //   .attr("x2", covidEndX)
+    //   .attr("y1", 0)
+    //   .attr("y2", height)
+    //   .attr("stroke", "black")
+    //   .attr("stroke-width", 2)
+    //   .attr("stroke-dasharray", "4");
+
+    // // label
+    // g.append("text")
+    //   .attr("x", covidEndX)
+    //   .attr("y", -10)
+    //   .attr("text-anchor", "middle")
+    //   .attr("fill", "black")
+    //   .style("font-size", "12px")
+    //   .text("COVID Ends");
+
+    const filteredData = processedData.filter(d => d.level === selectedLevel && d.country === country);
+    const maleData = filteredData.filter(d => d.sex === "Males" && d.age === "Total");
+    const femaleData = filteredData.filter(d => d.sex === "Females" && d.age === "Total");
+
+    yScale.domain([0, getMaxEnrollments()]);
+
+    g.select(".yaxis")
+      .transition()
+      .duration(1000)
+      .ease(d3.easeCubicInOut)
+      .call(d3.axisLeft(yScale))
+      .selectAll("text")
+      .style("font-size", "14px")
+      .style("fill", "black");
+
+    femaleData.forEach((female, i) => {
+      const male = maleData[i];
+      drawLines(g, male, female, xScale, yScale);
+    });
+
+    const maleDots = g.selectAll(".dot.male")
+      .data(maleData);
+
+    drawMalePoints(maleDots, xScale, yScale);
+
+    const femaleDots = g.selectAll(".dot.female")
+      .data(femaleData);
+
+    drawFemalePoints(femaleDots, xScale, yScale);
+  }
+
+  updateChart(selectedCountry);
+
+  selector.on("change", function () {
+    selectedCountry = this.value;
+    updateChart(this.value);
+  });
 }
 
 function easeOutLines(g) {
--- src/js/education_jobs/dumbbellUnenployment.js
+++ src/js/education_jobs/dumbbellUnenployment.js
@@ -1,197 +1,196 @@
-import { covidDates } from '../utils.js'
+import { covidDates, datasets } from '../utils.js'
 
 function dumbbellUnenployments() {
 
-  d3.csv("../../../dataset/UNENPLOYMENT/clean/estat_une_rt_m_filtered.csv")
-    .then(unenmploymentData => {
-      const MAX_ENROLLMENTS_COUNTRY = "European Union";
-      const years = d3.range(2016, 2025);
-      const months = d3.range(1, 13);
+  const unenmploymentData = datasets.unenploymentData;
 
-      let selectedCountry = "Italy";
-      let selectedUnit = "Thousand persons";
-      let selectedAge = "Total";
-      let sexIsTotal = false;
+  const MAX_ENROLLMENTS_COUNTRY = "European Union";
+  const years = d3.range(2016, 2025);
+  const months = d3.range(1, 13);
 
-      const selector = d3.select("#country-selector-dumbbell-unenmployments");
-      const allCountries = Array.from(new Set(unenmploymentData.map(d => d.country)));
+  let selectedCountry = "Italy";
+  let selectedUnit = "Thousand persons";
+  let selectedAge = "Total";
+  let sexIsTotal = false;
 
-      selector.selectAll("option")
-        .data(allCountries)
-        .enter()
-        .append("option")
-        .text(d => d)
-        .property("selected", d => d === selectedCountry);
+  const selector = d3.select("#country-selector-dumbbell-unenmployments");
+  const allCountries = Array.from(new Set(unenmploymentData.map(d => d.country)));
 
-      const processedData = unenmploymentData.flatMap(d =>
-        years.map(year => {
-          const monthlyValues = months.map(month => +d[`${year}-${month.toString().padStart(2, '0')}`]);
-          const meanValue = d3.mean(monthlyValues);
-          return {
-            country: d.country,
-            sex: d.sex,
-            age: d.age,
-            unit: d.unit,
-            year: year,
-            value: meanValue
-          };
-        })
-      );
+  selector.selectAll("option")
+    .data(allCountries)
+    .enter()
+    .append("option")
+    .text(d => d)
+    .property("selected", d => d === selectedCountry);
 
-      const margin = { top: 20, right: 20, bottom: 40, left: 80 };
-      const width = 800 - margin.left - margin.right;
-      const height = 600 - margin.top - margin.bottom;
-
-      const svg = d3.select("#dumbbell-unenployments")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
-      const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
-
-      const xScale = d3.scaleBand()
-        .domain(years)
-        .range([0, width])
-        .padding(0.2);
-
-      const getMaxValue = () => {
-        if (selectedCountry === "United States")
-          return 8000;
-        if (selectedCountry === "Turkey")
-          return 3500;
-        if (selectedCountry !== MAX_ENROLLMENTS_COUNTRY)
-          return 2000;
-
-        const maxVal = d3.max(years, year => {
-          const yearData = processedData.filter(d => d.year === year && d.country === selectedCountry && d.unit === selectedUnit && d.age === selectedAge);
-          if (sexIsTotal) {
-            return d3.max(yearData, d => d.value);
-          } else {
-            const maleValues = d3.max(yearData.filter(d => d.sex === "Males"), d => d.value);
-            const femaleValues = d3.max(yearData.filter(d => d.sex === "Females"), d => d.value);
-            return d3.max([maleValues, femaleValues]);
-          }
-        });
-        return maxVal;
+  const processedData = unenmploymentData.flatMap(d =>
+    years.map(year => {
+      const monthlyValues = months.map(month => +d[`${year}-${month.toString().padStart(2, '0')}`]);
+      const meanValue = d3.mean(monthlyValues);
+      return {
+        country: d.country,
+        sex: d.sex,
+        age: d.age,
+        unit: d.unit,
+        year: year,
+        value: meanValue
       };
+    })
+  );
 
-      const yScale = d3.scaleLinear()
-        .domain([0, getMaxValue()])
-        .range([height, 0]);
+  const margin = { top: 20, right: 20, bottom: 40, left: 80 };
+  const width = 800 - margin.left - margin.right;
+  const height = 600 - margin.top - margin.bottom;
 
-      g.append("g")
-        .attr("class", "xaxis")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(xScale)
-          .tickFormat(d3.format("d"))
-          .tickValues(years))
-        .selectAll("text")
-        .style("font-size", "14px")
-        .style("fill", "black");
+  const svg = d3.select("#dumbbell-unenployments")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+  const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-      g.append("g")
-        .attr("class", "yaxis")
-        .call(d3.axisLeft(yScale))
-        .selectAll("text")
-        .style("font-size", "14px")
-        .style("fill", "black");
+  const xScale = d3.scaleBand()
+    .domain(years)
+    .range([0, width])
+    .padding(0.2);
 
-      // Set axis line and tick color to black
-      g.selectAll(".xaxis path, .xaxis line, .yaxis path, .yaxis line")
-        .style("stroke", "black");
+  const getMaxValue = () => {
+    if (selectedCountry === "United States")
+      return 8000;
+    if (selectedCountry === "Turkey")
+      return 3500;
+    if (selectedCountry !== MAX_ENROLLMENTS_COUNTRY)
+      return 2000;
 
-      // Draw vertical lines for COVID start and end dates
-      const covidStartX = xScale(covidDates.start.getFullYear()) + xScale.bandwidth() / 2;
-      const covidEndX = xScale(covidDates.end.getFullYear()) + xScale.bandwidth() / 2;
-
-      // Y label
-      g.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -margin.left)
-        .attr("x", -height / 2)
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .style("fill", "black")
-        .style("font-size", "18px")
-        .text("Unemployment (thousands persons)");
-
-      function updateChart(country) {
-        easeOutLinesUnenmploy(g);
-
-        // covid lines
-        g.append("line")
-          .attr("x1", covidStartX)
-          .attr("x2", covidStartX)
-          .attr("y1", 0)
-          .attr("y2", height)
-          .attr("stroke", "black")
-          .attr("stroke-width", 2)
-          .attr("stroke-dasharray", "4");
-
-        // label
-        g.append("text")
-          .attr("x", covidStartX)
-          .attr("y", -10)
-          .attr("text-anchor", "middle")
-          .attr("fill", "black")
-          .style("font-size", "12px")
-          .text("COVID Starts");
-
-        g.append("line")
-          .attr("x1", covidEndX)
-          .attr("x2", covidEndX)
-          .attr("y1", 0)
-          .attr("y2", height)
-          .attr("stroke", "black")
-          .attr("stroke-width", 2)
-          .attr("stroke-dasharray", "4");
-
-        // label
-        g.append("text")
-          .attr("x", covidEndX)
-          .attr("y", -10)
-          .attr("text-anchor", "middle")
-          .attr("fill", "black")
-          .style("font-size", "12px")
-          .text("COVID Ends");
-
-        const filteredData = processedData.filter(d => d.country === country && d.unit === selectedUnit && d.age === selectedAge);
-        const maleData = filteredData.filter(d => d.sex === "Males");
-        const femaleData = filteredData.filter(d => d.sex === "Females");
-
-        yScale.domain([0, getMaxValue()]);
-
-        g.select(".yaxis")
-          .transition()
-          .duration(1000)
-          .ease(d3.easeCubicInOut)
-          .call(d3.axisLeft(yScale))
-          .selectAll("text")
-          .style("font-size", "14px")
-          .style("fill", "black");
-
-        femaleData.forEach((female, i) => {
-          const male = maleData[i];
-          drawLinesUnenploy(g, male, female, xScale, yScale);
-        });
-
-        const maleDots = g.selectAll(".dot.male")
-          .data(maleData);
-
-        drawMalePointsUnenploy(maleDots, xScale, yScale);
-
-        const femaleDots = g.selectAll(".dot.female")
-          .data(femaleData);
-
-        drawFemalePointsUnenploy(femaleDots, xScale, yScale);
+    const maxVal = d3.max(years, year => {
+      const yearData = processedData.filter(d => d.year === year && d.country === selectedCountry && d.unit === selectedUnit && d.age === selectedAge);
+      if (sexIsTotal) {
+        return d3.max(yearData, d => d.value);
+      } else {
+        const maleValues = d3.max(yearData.filter(d => d.sex === "Males"), d => d.value);
+        const femaleValues = d3.max(yearData.filter(d => d.sex === "Females"), d => d.value);
+        return d3.max([maleValues, femaleValues]);
       }
-
-      updateChart(selectedCountry);
-
-      selector.on("change", function () {
-        selectedCountry = this.value;
-        updateChart(this.value);
-      });
     });
+    return maxVal;
+  };
+
+  const yScale = d3.scaleLinear()
+    .domain([0, getMaxValue()])
+    .range([height, 0]);
+
+  g.append("g")
+    .attr("class", "xaxis")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale)
+      .tickFormat(d3.format("d"))
+      .tickValues(years))
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("fill", "black");
+
+  g.append("g")
+    .attr("class", "yaxis")
+    .call(d3.axisLeft(yScale))
+    .selectAll("text")
+    .style("font-size", "14px")
+    .style("fill", "black");
+
+  // Set axis line and tick color to black
+  g.selectAll(".xaxis path, .xaxis line, .yaxis path, .yaxis line")
+    .style("stroke", "black");
+
+  // Draw vertical lines for COVID start and end dates
+  const covidStartX = xScale(covidDates.start.getFullYear()) + xScale.bandwidth() / 2;
+  const covidEndX = xScale(covidDates.end.getFullYear()) + xScale.bandwidth() / 2;
+
+  // Y label
+  g.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left)
+    .attr("x", -height / 2)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("fill", "black")
+    .style("font-size", "18px")
+    .text("Unemployment (thousands persons)");
+
+  function updateChart(country) {
+    easeOutLinesUnenmploy(g);
+
+    // covid lines
+    g.append("line")
+      .attr("x1", covidStartX)
+      .attr("x2", covidStartX)
+      .attr("y1", 0)
+      .attr("y2", height)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", "4");
+
+    // label
+    g.append("text")
+      .attr("x", covidStartX)
+      .attr("y", -10)
+      .attr("text-anchor", "middle")
+      .attr("fill", "black")
+      .style("font-size", "12px")
+      .text("COVID Starts");
+
+    g.append("line")
+      .attr("x1", covidEndX)
+      .attr("x2", covidEndX)
+      .attr("y1", 0)
+      .attr("y2", height)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+      .attr("stroke-dasharray", "4");
+
+    // label
+    g.append("text")
+      .attr("x", covidEndX)
+      .attr("y", -10)
+      .attr("text-anchor", "middle")
+      .attr("fill", "black")
+      .style("font-size", "12px")
+      .text("COVID Ends");
+
+    const filteredData = processedData.filter(d => d.country === country && d.unit === selectedUnit && d.age === selectedAge);
+    const maleData = filteredData.filter(d => d.sex === "Males");
+    const femaleData = filteredData.filter(d => d.sex === "Females");
+
+    yScale.domain([0, getMaxValue()]);
+
+    g.select(".yaxis")
+      .transition()
+      .duration(1000)
+      .ease(d3.easeCubicInOut)
+      .call(d3.axisLeft(yScale))
+      .selectAll("text")
+      .style("font-size", "14px")
+      .style("fill", "black");
+
+    femaleData.forEach((female, i) => {
+      const male = maleData[i];
+      drawLinesUnenploy(g, male, female, xScale, yScale);
+    });
+
+    const maleDots = g.selectAll(".dot.male")
+      .data(maleData);
+
+    drawMalePointsUnenploy(maleDots, xScale, yScale);
+
+    const femaleDots = g.selectAll(".dot.female")
+      .data(femaleData);
+
+    drawFemalePointsUnenploy(femaleDots, xScale, yScale);
+  }
+
+  updateChart(selectedCountry);
+
+  selector.on("change", function () {
+    selectedCountry = this.value;
+    updateChart(this.value);
+  });
 }
 
 function easeOutLinesUnenmploy(g) {
