5571828d0f1e16cba92f6117d2a9f1dd103a7132
dariovalentinii <dario.vale01@gmail.com>
2025-01-24 12:11:41 +0100

    waffle: anni e filtri causa morte

--- src/js/healthcare/waffleChartHealth.js
+++ src/js/healthcare/waffleChartHealth.js
@@ -2,9 +2,6 @@ import { datasets } from "../utils.js";
 
 function waffleChart() {
     const deaths = datasets.mentalHealthData;
-    const width = 1200;
-    const height = 600;
-    const margin = { top: 50, right: 40, bottom: 40, left: 40 };
 
     const numericColumns = Object.keys(deaths[0]).filter(key => !isNaN(key));
     deaths.forEach(d => {
@@ -13,25 +10,38 @@ function waffleChart() {
         });
     });
 
-
-    const ages = [...new Set(deaths.map(d => d.age))];
+    const parentContainer = d3.select("#waffle-chart-container").node().parentNode;
+    const width = parentContainer.getBoundingClientRect().width * 0.9;
+    const height = 600;
+    const margin = { top: 50, right: 40, bottom: 40, left: 40 };
 
     const svg = d3.select("#waffle-chart-container")
         .append("svg")
         .attr("width", width)
-        .attr("height", height)
-        .append("g");
+        .attr("height", height);
 
-    const colorMap = {
-        "COVID-19": "lightblue",
-        "Cancer": "lightgreen",
-        "Heart disease": "lightcoral",
-        "Stroke": "lightyellow",
-        "Diabetes": "lightpink",
-        "Alzheimer's disease": "lightgrey",
-        "Influenza and pneumonia": "lightcyan",
-        "Kidney disease": "lightgoldenrodyellow"
+    // Getting the values of the selected categories
+    const categories_checkbox = Array.from(document.querySelectorAll('input[name="waffle-metric"]'));
+    const categories_number = categories_checkbox.length;
+
+    const indexMap = new Map(
+        categories_checkbox.map((checkbox, index) => [checkbox.value, index])
+    );
+
+    const colorMap = new Map(
+        categories_checkbox.map((checkbox, index) => [index, { color: d3.schemeCategory10[index % 10], cause: checkbox.value }])
+    );
+    colorMap.set(categories_number, { color: "lightgrey", cause: "Empty" });
+
+
+    function getCauseIndex(cause) {
+        return indexMap.get(cause);
     }
+
+    function fillColor(d) {
+        return colorMap.get(d).color;
+    }
+
 
     // Setting the values of the year selectors
     const year1Select = document.getElementById('waffle-year-1');
@@ -52,44 +62,31 @@ function waffleChart() {
     year1Select.value = "2019";
     year2Select.value = "2021";
 
-    // Getting the values of the selected categories
-    const categories_checkbox = Array.from(document.querySelectorAll('input[name="waffle-metric"]'));
-
+    function drawEmptyWaffle(chart) {
+        chart.attr("transform", `translate(${chart == chart1 ? xOffset : width - xOffset - cols * (gridSize + gap)}, ${margin.top})`)
+            .selectAll("rect")
+            .data(initialWaffleData)
+            .enter().append("rect")
+            .attr("x", (d, i) => (i % cols) * (gridSize + gap))
+            .attr("y", (d, i) => (cols - 1 - Math.floor(i / cols)) * (gridSize + gap))
+            .attr("width", gridSize)
+            .attr("height", gridSize)
+            .attr("fill", d => fillColor(d))
+            .attr("rx", 5)
+            .attr("ry", 5);
+    }
 
     // Initial draw of the waffles
-    const gridSize = 40;
+    const gridSize = width * 0.04;
     const gap = 2; // Add a gap between squares
     const cols = 10;
 
     const xOffset = width * 0.05;
-    const initialWaffleData = Array(cols * cols).fill(0);
-    const chart1 = svg.append("g")
-        .attr("transform", `translate(${xOffset}, ${margin.top})`);
-
-    chart1.selectAll("rect")
-        .data(initialWaffleData)
-        .enter().append("rect")
-        .attr("x", (d, i) => (i % cols) * (gridSize + gap))
-        .attr("y", (d, i) => (cols - 1 - Math.floor(i / cols)) * (gridSize + gap))
-        .attr("width", gridSize)
-        .attr("height", gridSize)
-        .attr("fill", "lightgrey")
-        .attr("rx", 5)
-        .attr("ry", 5);
-
-    const chart2 = svg.append("g")
-        .attr("transform", `translate(${width - xOffset - cols * (gridSize + gap)}, ${margin.top})`);
-
-    chart2.selectAll("rect")
-        .data(initialWaffleData)
-        .enter().append("rect")
-        .attr("x", (d, i) => (i % cols) * (gridSize + gap))
-        .attr("y", (d, i) => (cols - 1 - Math.floor(i / cols)) * (gridSize + gap))
-        .attr("width", gridSize)
-        .attr("height", gridSize)
-        .attr("fill", "lightgrey")
-        .attr("rx", 5)
-        .attr("ry", 5);
+    const initialWaffleData = Array(cols * cols).fill(categories_number);
+    const chart1 = svg.append("g");
+    drawEmptyWaffle(chart1);
+    const chart2 = svg.append("g");
+    drawEmptyWaffle(chart2);
 
 
     // Update the waffle charts 
@@ -100,7 +97,6 @@ function waffleChart() {
         const currentCountry = deaths[0].country;
 
 
-        const category = "COVID-19";
         const sex = "Total";
         const age = "Total";
 
@@ -118,24 +114,82 @@ function waffleChart() {
                 return filteredData;
             });
 
-        const percentage = filteredYearData.find(d => d.cause === category && d.sex == sex && d.age == age)[`percentage_${year}`];
-        const numSquares = Math.round((percentage / 100) * (cols * cols));
-        const waffleData = Array(cols * cols).fill(0).map((d, i) => i < numSquares ? 1 : 0);
 
-        // svg.select(`g:nth-child(${number})`).remove();
+        let waffleData = Array(cols * cols).fill(categories_number); // Initialize with "Empty" category
+
+        active_categories.forEach(category => {
+            const percentage = filteredYearData.find(d => d.cause === category && d.sex == sex && d.age == age)[`percentage_${year}`];
+            const numSquares = Math.round((percentage / 100) * (cols * cols));
+            const causeIndex = getCauseIndex(category);
+
+            for (let i = 0; i < numSquares; i++) {
+                const emptyIndex = waffleData.indexOf(categories_number); // Find the first empty slot
+                if (emptyIndex !== -1) {
+                    waffleData[emptyIndex] = causeIndex;
+                }
+            }
+        });
+
         chart.selectAll("rect")
             .data(waffleData)
-            .attr("fill", d => d === 1 ? "lightblue" : "lightgrey");
-
-        chart.append("text")
-            .attr("x", cols * (gridSize + gap) / 2)
-            .attr("y", -10)
-            .attr("text-anchor", "middle")
-            .text(`Year: ${year}`);
+            .attr("fill", d => fillColor(d));
     }
 
     updateWaffleChart(2019, chart1);
     updateWaffleChart(2021, chart2);
+
+    // Checkbox event listeners
+    categories_checkbox.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            const checkedCheckboxes = categories_checkbox.filter(d => d.checked);
+
+            if (checkedCheckboxes.length === 1) {
+                checkedCheckboxes[0].style.cursor = 'not-allowed';
+            } else {
+                categories_checkbox.forEach(d => d.style.cursor = 'pointer');
+            }
+
+            if (checkedCheckboxes.length === 0) {
+                checkbox.checked = true;
+                checkbox.style.cursor = 'not-allowed';
+            }
+            else {
+                updateWaffleChart(year1Select.value, chart1);
+                updateWaffleChart(year2Select.value, chart2);
+            }
+
+        });
+    });
+
+    /********** Year selectors event listeners **********/
+    function disableYear1Options() {
+        const selectedYear2 = year2Select.value;
+        Array.from(year1Select.options).forEach(option => {
+            option.disabled = option.value === selectedYear2;
+        });
+    }
+
+    function disableYear2Options() {
+        const selectedYear1 = year1Select.value;
+        Array.from(year2Select.options).forEach(option => {
+            option.disabled = option.value === selectedYear1;
+        });
+    }
+
+    year1Select.addEventListener('change', () => {
+        disableYear2Options();
+        updateWaffleChart(year1Select.value, chart1);
+    });
+
+    year2Select.addEventListener('change', () => {
+        disableYear1Options();
+        updateWaffleChart(year2Select.value, chart2);
+    });
+
+    disableYear1Options();
+    disableYear2Options();
+    /****************************************************/
+
 }
 
 waffleChart();
