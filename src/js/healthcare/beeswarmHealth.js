4fba2be69484c12850e2d71b0ff5b13fb27bdac0
dariovalentinii <dario.vale01@gmail.com>
2025-01-28 15:10:41 +0100

    cose varie

--- src/js/healthcare/beeswarmHealth.js
+++ src/js/healthcare/beeswarmHealth.js
@@ -2,44 +2,32 @@ import { datasets } from "../utils.js";
 
 function beeswarm() {
     const mentalDeaths = datasets.mentalHealthData
-        .filter(d => d.cause === "Mental and behavioural disorders" && d.age == "Total" && d.sex == "Total")
+        .filter(d => d.cause === "Mental and behavioural disorders" && d.age == "Total" && d.sex == "Total" && d.deaths != null)
         .map(d => ({
             country: d.country,
             year: +d.year,
             deaths: +d.deaths,
-            percentage: +d.percentage
+            percDeaths: +d.percentage
         }));
 
+    const expendituresData = datasets.expendituresData;
 
-    const covidData = datasets.covidData.year_new.cases.map(d => ({
-        country: d.country,
-        year: +d.year,
-        newCases: +d.new_cases
-    })).filter(d => d.year >= 2020 && d.year <= 2024);
+    const data = [];
 
-    const countriesMental = [...new Set(mentalDeaths.map(d => `${d.country}-${d.year}`))];
-    const countriesCovid = [...new Set(covidData.map(d => `${d.country}-${d.year}`))];
-    const countries = countriesMental.filter(c => countriesCovid.includes(c)).map(c => {
-        const [country, year] = c.split("-");
-        return { country, year: +year };
+    mentalDeaths.forEach(md => {
+        const entry = expendituresData.find(ed => ed.country === md.country && +ed.year === +md.year);
+        if (entry) {
+            data.push({
+                country: md.country,
+                year: +md.year,
+                deaths: +md.deaths,
+                percDeaths: +md.percDeaths.toFixed(2),
+                healthExp: +entry.health,
+                totalExp: +entry.total,
+                percExp: +entry.percentage
+            });
+        }
     });
-
-    console.log(countries)
-
-    const data = countries.map((country, year) => {
-        const mentalEntry = mentalDeaths.find(md => md.country === country && +md.year === +year);
-        const covidEntry = covidData.find(cd => cd.country === country && +cd.year === +year);
-        console.log(mentalEntry, covidEntry);
-        return {
-            country: country,
-            year: +year,
-            newCases: +covidEntry.newCases,
-            deaths: +mentalEntry.deaths,
-            deathsPercentage: +mentalEntry.percentage
-        };
-    });
-
-    console.log(data);
 
     const width = 1000;
     const height = 600;
@@ -54,45 +42,14 @@ function beeswarm() {
 
     // Scales
     const xScale = d3.scaleLinear()
-        .domain([0, d3.max(mentalDeaths.map(d => d.percentage))]) // Assuming percentages range from 0 to 100
+        .domain([0, d3.max(data.map(d => +d.percDeaths))]) // Assuming percentages range from 0 to 100
         .range([0, width]);
 
+
     const radiusScale = d3.scaleSqrt()
-        .domain([0, d3.max(covidData, d => d.newCases)]) // Map totalCases to circle size
-        .range([3, 20]); // Circle radius range
+        .domain([0, d3.max(data, d => +d.healthExp)]) // Map totalCases to circle size
+        .range([3, 40]); // Circle radius range
 
-    const years = [...new Set(mentalDeaths.map(d => d.year))];
-    let year = d3.min(years);
-
-    const simulation = d3.forceSimulation()
-        .force("x", d3.forceX(d => xScale(d.percentage)).strength(1))
-        .force("y", d3.forceY(height / 2).strength(0.05))
-        .force("collide", d3.forceCollide(d => radiusScale(d.totalCases) + 1))
-        .stop();
-
-    function updateChart(year) {
-        const filteredData = data.filter(d => d.year === year);
-
-        simulation.nodes(filteredData);
-        for (let i = 0; i < 300; i++) simulation.tick();
-
-        const circles = svg.selectAll("circle").data(filteredData, d => d.country);
-
-        circles.enter()
-            .append("circle")
-            .merge(circles)
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y)
-            .attr("r", d => radiusScale(d.totalCases))
-            .style("fill", "steelblue")
-            .style("opacity", 0.8)
-            .style("stroke", "#333");
-
-        circles.exit().remove();
-    }
-
-    // Initial rendering
-    updateChart(year);
 
     // Axes
     svg.append("g")
@@ -103,7 +60,70 @@ function beeswarm() {
         .attr("x", width / 2)
         .attr("y", height + 40)
         .style("text-anchor", "middle")
-        .text("Percentage of Deaths Due to Mental Disorders");
+        .text("Percentage of Deaths Due to Mental Disorders")
+        .style("color", "black");
+
+    svg.selectAll("path, line").style("stroke", "black").style("color", "black");
+    svg.selectAll(" .tick").style("color", "black");
+
+    const years = Array.from(new Set(data.map(d => +d.year)));
+    let year = d3.min(years);
+
+    const simulation = d3.forceSimulation()
+        .force("x", d3.forceX(d => xScale(d.percDeaths)).strength(1))
+        .force("y", d3.forceY(height / 2).strength(0.05))
+        .force("collide", d3.forceCollide(d => radiusScale(+d.healthExp) + 1))
+        .stop();
+
+    const tooltip = d3.select('#waffle-chart-container').append('div')
+        .attr('class', 'tooltip-waffle-health flex flex-col items-center')
+        .style('position', 'absolute')
+        .style('background', '#fff')
+        .style('padding', '8px')
+        .style('border', '1px solid #000')
+        .style('border-radius', '4px')
+        .style('pointer-events', 'none')
+        .style('opacity', 0)
+        .style('color', 'black');
+
+    function updateChart(year) {
+        const filteredData = data.filter(d => +d.year === +year);
+
+        simulation.nodes(filteredData);
+        for (let i = 0; i < filteredData.length; i++) simulation.tick();
+
+        const circles = svg.selectAll("circle").data(filteredData);
+
+        circles.enter()
+            .append("circle")
+            .merge(circles)
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y)
+            .attr("r", d => radiusScale(+d.healthExp))
+            .style("fill", "steelblue")
+            .style("opacity", 0.8)
+            .style("stroke", "#333");
+
+        circles.exit().remove();
+
+        svg.selectAll("circle")
+            .on("mouseover", (event, d) => {
+                tooltip.style("display", "block")
+                    .html(`
+                        <strong>Country:</strong> ${d.country}<br>
+                        <strong>Deaths (%):</strong> ${d.percDeaths}<br>
+                        <strong>Health expenditure:</strong> ${d3.format(",")(+d.healthExp)}`)
+                    .style("color", "black");
+            })
+            .on("mousemove", (event) => {
+                tooltip.style("top", `${event.pageY + 10}px`)
+                    .style("left", `${event.pageX + 10}px`);
+            })
+            .on("mouseout", () => tooltip.style("display", "none"));
+    }
+
+    // Initial rendering
+    updateChart(year);
 
     // Slider
     // const slider = d3.select("body")
@@ -118,30 +138,6 @@ function beeswarm() {
     //     year = +this.value;
     //     updateChart(year);
     // });
-
-    // Tooltips
-    const tooltip = d3.select("body").append("div")
-        .style("position", "absolute")
-        .style("background", "white")
-        .style("border", "1px solid #ccc")
-        .style("padding", "5px")
-        .style("display", "none")
-        .style("pointer-events", "none");
-
-    svg.selectAll("circle")
-        .on("mouseover", (event, d) => {
-            tooltip.style("display", "block")
-                .html(`
-        <strong>Country:</strong> ${d.country}<br>
-        <strong>Deaths (%):</strong> ${d.deathsPercentage}<br>
-        <strong>Total Cases:</strong> ${d3.format(",")(d.totalCases)}
-      `);
-        })
-        .on("mousemove", (event) => {
-            tooltip.style("top", `${event.pageY + 10}px`)
-                .style("left", `${event.pageX + 10}px`);
-        })
-        .on("mouseout", () => tooltip.style("display", "none"));
 }
 
 beeswarm();
